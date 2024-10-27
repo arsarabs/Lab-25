cbd415251d1533f643be62cb5febe03b35eba7e0
Your Name <your-email@example.com>
2024-10-27 09:17:34 -0700

    prototypes

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,30 @@
 //Anthony Sarabia
 //
-#include <iostream>
-#include <chrono>
+#include <iostream>          
+#include <chrono>            
+#include <vector>            
+#include <list>              
+#include <set>               
+#include <fstream>           
+#include <string>            
+#include <iomanip>          
+#include <algorithm>         // For std::sort
 using namespace std;
+using namespace std::chrono; 
+
+//CONSTANTS
+// Constants
+const string FILENAME = "codes.txt";     // Name of the input file containing codes
+const int NUM_ELEMENTS = 20000;         // Total number of elements to process
+const string INSERT_VALUE = "TESTING";  // Value to insert into data structures
+
+//Function Prototypes
+long long sortList(list<string>& l);
+long long sortVector(vector<string>& v);
+long long readSet(set<string>& s);
+long long readList(list<string>& l);
+long long readVector(vector<string>& v);
+
 
 int main() {
 
@@ -10,9 +32,3 @@ int main() {
     return 0;
 }
 
-/* syntax examples:
-auto start = high_resolution_clock::now()
-auto end = high_resolution_clock::now()
-auto duration = duration_cast<milliseconds>(end - start)
-duration.count() references elapsed milliseconds
-*/
